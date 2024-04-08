b32779657e66f0a8595c47bfff29aec1eb9a4724
김태용 <94teo@naver.com>
2024-04-08 13:42:59 +0900

    ✨ Add TypeScript project files

---
+++ 5. TypeScript/section2/src/chapter1.ts
@@ -0,0 +1,26 @@
+// Number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "Hello, TypeScript";
+let str2: string = "Hello, TypeScript";
+let str3: string = `Hello, TypeScript`;
+let str4: string = `Hello, TypeScript ${num1}`;
+
+// boolean
+let boo1: boolean = true;
+let boo2: boolean = false;
+
+// null
+let null1: null = null;
+
+//undefined
+let unde1: undefined = undefined;
+
+let numA: number = null;
